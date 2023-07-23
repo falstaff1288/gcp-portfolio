13bb11987c283a11b09fc9e1d0f709ca15f04244
Denis Saintilma <denis.saintilma1@gmail.com>
2023-07-23 13:16:01 -0400

    Create projects for the portfolio

---
+++ projects.tf
@@ -0,0 +1,11 @@
+resource "google_project" "portfolio_dev" {
+  name       = "densnet-portfolio-dev"
+  project_id = "densnet-portfolio-dev"
+  org_id     = "914921624150"
+}
+
+resource "google_project" "portfolio_prod" {
+  name       = "densnet-portfolio-prod"
+  project_id = "densnet-portfolio-prod"
+  org_id     = "914921624150"
+}
