5be16c06c359091fbe1a6f25c2c428c7ae12d31d
Denis Saintilma <denis.saintilma1@gmail.com>
2023-07-23 18:34:05 -0400

    Adding a third bucket

--- storage.tf
+++ storage.tf
@@ -15,3 +15,12 @@ resource "google_storage_bucket" "test_bucket1" {
 
   public_access_prevention = "enforced"
 }
+
+resource "google_storage_bucket" "test_bucket2" {
+  name          = "densnet-test-bucket2-${terraform.workspace}"
+  location      = upper(var.region)
+  storage_class = "STANDARD"
+  project = var.project[terraform.workspace]
+
+  public_access_prevention = "enforced"
+}
