4503691b8647ad1bed56bd2d4cde0dc3a849e0ce
Denis Saintilma <denis.saintilma1@gmail.com>
2023-07-23 16:23:56 -0400

    Set vars

--- storage.tf
+++ storage.tf
@@ -2,6 +2,7 @@ resource "google_storage_bucket" "test_bucket" {
   name          = "densnet-test-bucket"
   location      = upper(var.region[terraform.workspace])
   storage_class = "STANDARD"
+  project = var.project[terraform.workspace]
 
   public_access_prevention = "enforced"
 }
