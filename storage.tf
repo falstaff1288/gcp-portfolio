300d816855815434c15b6fa8f5f856b53ed16ae3
Denis Saintilma <denis.saintilma1@gmail.com>
2023-07-23 16:21:09 -0400

    ADd location and storage class

--- storage.tf
+++ storage.tf
@@ -1,6 +1,7 @@
 resource "google_storage_bucket" "test_bucket" {
   name          = "densnet-test-bucket"
-  location      = "US-CENTRAL1"
+  location      = upper(var.project[terraform.workspace])
+  storage_class = "STANDARD"
 
   public_access_prevention = "enforced"
 }
