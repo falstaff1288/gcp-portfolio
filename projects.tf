41229e0fe708aa9dca7efe8775f4591bddefb5bd
Denis Saintilma <denis.saintilma1@gmail.com>
2023-07-23 20:47:30 -0400

    Temp delete project

--- projects.tf
+++ projects.tf
@@ -10,22 +10,22 @@
 #   org_id     = "914921624150"
 # }
 
-module "project_densnet_shared_vpc_host_test" {
-  source  = "terraform-google-modules/project-factory/google"
-  version = "~> 14.2"
+# module "project_densnet_shared_vpc_host_test" {
+#   source  = "terraform-google-modules/project-factory/google"
+#   version = "~> 14.2"
 
-  name                 = "densnet-host-vpc-test"
-  random_project_id    = true
-  org_id               = "914921624150"
-#   usage_bucket_name    = "densnet-shared-network-test-usage-report-bucket"
-#   usage_bucket_prefix  = "pf/test/1/integration"
-  billing_account      = "017CB9-ECCEBC-A2CCF8"
-#   svpc_host_project_id = "shared_vpc_host_name"
+#   name                 = "densnet-host-vpc-test"
+#   random_project_id    = true
+#   org_id               = "914921624150"
+# #   usage_bucket_name    = "densnet-shared-network-test-usage-report-bucket"
+# #   usage_bucket_prefix  = "pf/test/1/integration"
+#   billing_account      = "017CB9-ECCEBC-A2CCF8"
+# #   svpc_host_project_id = "shared_vpc_host_name"
 
-#   shared_vpc_subnets = [
-#     "projects/base-project-196723/regions/us-east1/subnetworks/default",
-#     "projects/base-project-196723/regions/us-central1/subnetworks/default",
-#     "projects/base-project-196723/regions/us-central1/subnetworks/subnet-1",
-#   ]
+# #   shared_vpc_subnets = [
+# #     "projects/base-project-196723/regions/us-east1/subnetworks/default",
+# #     "projects/base-project-196723/regions/us-central1/subnetworks/default",
+# #     "projects/base-project-196723/regions/us-central1/subnetworks/subnet-1",
+# #   ]
 
-}
+# }
