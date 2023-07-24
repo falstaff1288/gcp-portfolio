80428683fcdf7c24c2b8b4faa35938572a1fd01b
Denis Saintilma <denis.saintilma1@gmail.com>
2023-07-23 20:57:32 -0400

    Remove test buckets

--- storage.tf
+++ storage.tf
@@ -1,26 +1,26 @@
-resource "google_storage_bucket" "test_bucket" {
-  name          = "densnet-test-bucket-${terraform.workspace}"
-  location      = upper(var.region)
-  storage_class = "STANDARD"
-  project       = var.project[terraform.workspace]
+# resource "google_storage_bucket" "test_bucket" {
+#   name          = "densnet-test-bucket-${terraform.workspace}"
+#   location      = upper(var.region)
+#   storage_class = "STANDARD"
+#   project       = var.project[terraform.workspace]
 
-  public_access_prevention = "enforced"
-}
+#   public_access_prevention = "enforced"
+# }
 
-resource "google_storage_bucket" "test_bucket1" {
-  name          = "densnet-test-bucket1-${terraform.workspace}"
-  location      = upper(var.region)
-  storage_class = "STANDARD"
-  project       = var.project[terraform.workspace]
+# resource "google_storage_bucket" "test_bucket1" {
+#   name          = "densnet-test-bucket1-${terraform.workspace}"
+#   location      = upper(var.region)
+#   storage_class = "STANDARD"
+#   project       = var.project[terraform.workspace]
 
-  public_access_prevention = "enforced"
-}
+#   public_access_prevention = "enforced"
+# }
 
-resource "google_storage_bucket" "test_bucket2" {
-  name          = "densnet-test-bucket2-${terraform.workspace}"
-  location      = upper(var.region)
-  storage_class = "STANDARD"
-  project       = var.project[terraform.workspace]
+# resource "google_storage_bucket" "test_bucket2" {
+#   name          = "densnet-test-bucket2-${terraform.workspace}"
+#   location      = upper(var.region)
+#   storage_class = "STANDARD"
+#   project       = var.project[terraform.workspace]
 
-  public_access_prevention = "enforced"
-}
+#   public_access_prevention = "enforced"
+# }
