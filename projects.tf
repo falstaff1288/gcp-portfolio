f3a7c8c558929484d22c12b93db9861e4d060f08
falstaff1288 <1068des@gmail.com>
2023-07-23 20:07:08 -0400

    Test project factory (#22)
    Co-authored-by: Denis Saintilma <denis.saintilma1@gmail.com>

--- projects.tf
+++ projects.tf
@@ -9,3 +9,23 @@
 #   project_id = "densnet-portfolio-prod"
 #   org_id     = "914921624150"
 # }
+
+module "project_densnet_shared_vpc_host_test" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "~> 14.2"
+
+  name                 = "densnet-shared-vpc-host-test"
+  random_project_id    = true
+  org_id               = "914921624150"
+#   usage_bucket_name    = "densnet-shared-network-test-usage-report-bucket"
+#   usage_bucket_prefix  = "pf/test/1/integration"
+  billing_account      = "017CB9-ECCEBC-A2CCF8"
+#   svpc_host_project_id = "shared_vpc_host_name"
+
+#   shared_vpc_subnets = [
+#     "projects/base-project-196723/regions/us-east1/subnetworks/default",
+#     "projects/base-project-196723/regions/us-central1/subnetworks/default",
+#     "projects/base-project-196723/regions/us-central1/subnetworks/subnet-1",
+#   ]
+
+}
