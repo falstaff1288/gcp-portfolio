2f838a23efb1004c758d17ebce550eb990d7c29d
Denis Saintilma <denis.saintilma1@gmail.com>
2023-07-24 00:30:41 -0400

    Setup Shared VPC for a project

--- projects.tf
+++ projects.tf
@@ -16,18 +16,56 @@ module "project_densnet_shared_vpc_host" {
 
   name              = "densnet-host-vpc-${terraform.workspace}"
   random_project_id = true
-  org_id            = "914921624150"
+  org_id            = var.organization
   folder_id         = "649529978270"
+  billing_account                = "017CB9-ECCEBC-A2CCF8"
+  enable_shared_vpc_host_project = true
+}
+
+module "shared_vpc_densnet_mgmt" {
+    source  = "terraform-google-modules/network/google"
+    version = "~> 7.1"
+
+    depends_on = [ module.module.project_densnet_shared_vpc_host ]
+
+    project_id = module.project_densnet_shared_vpc_host.project_id
+    network_name = "vpc-densnet-apps-${var.project[terraform.workspace]}"
+
+    subnets = [
+        {
+            subnet_name = "${var.project[terraform.workspace]}"
+            sumnet_ip = "10.140.0.0/20"
+            subnet_region = "us-central1"
+            description = "subnet for applications (${var.project[terraform.workspace]})"
+        }
+    ]
+}
+
+
+########
+
+resource "google_folder" "apps" {
+  display_name = "Applications"
+  parent       = "organizations/${var.organization}"
+}
+
+module "projects" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "~> 14.2"
+
+  depends_on = [ google_folder.apps, shared_vpc_densnet_mgmt ]
+
+  name              = var.project[terraform.workspace]
+  random_project_id = true
+  org_id            = var.organization
+  folder_id         = google_folder.apps.name
   #   usage_bucket_name    = "densnet-shared-network-test-usage-report-bucket"
   #   usage_bucket_prefix  = "pf/test/1/integration"
   billing_account                = "017CB9-ECCEBC-A2CCF8"
-  enable_shared_vpc_host_project = true
-  #   svpc_host_project_id = "shared_vpc_host_name"
+  svpc_host_project_id = module.project_densnet_shared_vpc_host.project_id
 
-  #   shared_vpc_subnets = [
-  #     "projects/base-project-196723/regions/us-east1/subnetworks/default",
-  #     "projects/base-project-196723/regions/us-central1/subnetworks/default",
-  #     "projects/base-project-196723/regions/us-central1/subnetworks/subnet-1",
-  #   ]
+    shared_vpc_subnets = [
+      "projects//regions/us-central1/subnetworks/default",
+    ]
 
 }
