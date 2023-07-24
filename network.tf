61483235d3716693eab9b9df32ec849afa66cb54
falstaff1288 <1068des@gmail.com>
2023-07-24 02:05:18 -0400

    Set stub to subnet_name (#25)
    * Set stub to subnet_name
    
    * Fix a bad module reference
    
    * Add module artifact
    
    * wip
    
    * Remove Cloud Router
    
    * wip
    
    * Add module artifact
    
    * wip
    
    * Remove Cloud Router
    
    * wip
    
    * Revert "Add module artifact"
    
    This reverts commit f84ea13d703173e35c39f8c1806fdfe6bc3bcc2f.
    
    * wip
    
    ---------
    
    Co-authored-by: Denis Saintilma <denis.saintilma1@gmail.com>

--- network.tf
+++ network.tf
@@ -2,31 +2,31 @@ module "project_densnet_shared_vpc_host" {
   source  = "terraform-google-modules/project-factory/google"
   version = "~> 14.2"
 
-  name              = "densnet-host-vpc-${terraform.workspace}"
-  random_project_id = true
-  org_id            = var.organization
-  folder_id         = "649529978270"
+  name                           = "densnet-host-vpc-${terraform.workspace}"
+  random_project_id              = true
+  org_id                         = var.organization
+  folder_id                      = "649529978270"
   billing_account                = "017CB9-ECCEBC-A2CCF8"
   enable_shared_vpc_host_project = true
 }
 
-module "shared_vpc_densnet_mgmt" {
-    source  = "terraform-google-modules/network/google"
-    version = "~> 7.1"
+module "shared_vpc_densnet_apps" {
+  source  = "terraform-google-modules/network/google"
+  version = "~> 7.1"
 
-    depends_on = [ module.project_densnet_shared_vpc_host ]
+  depends_on = [module.project_densnet_shared_vpc_host]
 
-    project_id = module.project_densnet_shared_vpc_host.project_id
-    network_name = "vpc-densnet-apps-${terraform.workspace}"
+  project_id   = module.project_densnet_shared_vpc_host.project_id
+  network_name = "vpc-densnet-apps-${terraform.workspace}"
 
-    subnets = [
-        {
-            subnet_name = "${var.project[terraform.workspace]}"
-            subnet_ip = "10.140.0.0/20"
-            subnet_region = "us-central1"
-            description = "subnet for applications (${var.project[terraform.workspace]})"
-        }
-    ]
+  subnets = [
+    {
+      subnet_name   = "${var.project[terraform.workspace]}"
+      subnet_ip     = "10.140.0.0/20"
+      subnet_region = "us-central1"
+      description   = "subnet for applications (${var.project[terraform.workspace]})"
+    }
+  ]
 }
 
 ### Cloud Router with NAT
@@ -35,10 +35,10 @@ module "cloud_router" {
   version = "~> 5.0"
 
   name    = "cloud-router-${var.region}"
-  project = module.project_densnet_shared_vpc_host.project_id
+  project = module.shared_vpc_densnet_apps.project_id
   region  = var.region
-  network = module.project_densnet_shared_vpc_host.project_name
+  network = module.shared_vpc_densnet_apps.network_name
   nats = [{
     name = "nat-${var.region}"
-    }]
+  }]
 }
