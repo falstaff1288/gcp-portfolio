f2d1dc29e96a809fc2912d6b60da23a1fe75eacb
Denis Saintilma <denis.saintilma1@gmail.com>
2023-07-26 08:09:01 -0400

    Remove old comment

--- modules/shared_vpc/network.tf
+++ modules/shared_vpc/network.tf
@@ -16,22 +16,3 @@ module "network_shared_vpc" {
       }
   ]
 }
-
-### Cloud Router with NAT
-# module "cloud_router" {
-#   source  = "terraform-google-modules/cloud-router/google"
-#   version = "~> 5.0"
-
-#   depends_on = [ module.network_shared_vpc ]
-
-#   name    = "cloud-router"
-#   project = module.shared_vpc[each.value.name].project_id
-#   region  = each.value.region
-#   network = module.shared_vpc_densnet_apps.network_name
-#   nats = [{
-#     name = "nat-${each.value.region}"
-#     # log_config = {
-#     #     enable = false
-#     # }
-#   }]
-# }
